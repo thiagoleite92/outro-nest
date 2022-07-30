c30140c499615f35c8b1ebc2f5b801945219b4a8
Thiago Leite <leite.tjs@gmail.com>
2022-07-29 21:58:42 -0300

    crud básico hardcoded

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,49 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateUserDto, ListUserDto } from './dto';
 import { IUserController } from './interface/user-controller.interface';
 
 @Controller('user')
 export class UserController implements IUserController {
+  constructor(private userService: UserService) {}
+
   @Get('list/:id')
   async listOne(@Param('id') id: ListUserDto): Promise<any> {
-    console.log(id);
-    return id;
+    const response = this.userService.listOne(id);
+    return response;
   }
+
   @Get('list')
   async listAll(): Promise<string> {
-    return ' all Users';
+    const response = this.userService.listAll();
+
+    return response;
   }
 
   @Post('create')
   async create(@Body() createUserDto: CreateUserDto): Promise<any> {
-    return { message: 'Usuario criado', new_user: createUserDto };
+    const response = this.userService.create(createUserDto);
+    return response;
+  }
+
+  @Put('edit/:id')
+  findAndUpdate(@Param('id') id: string, @Body() update: any): Promise<any> {
+    const response = this.userService.findAndUpdate(+id, update);
+
+    return response;
+  }
+
+  @Delete('delete/:id')
+  async remove(@Param('id') id: ListUserDto): Promise<any> {
+    const response = this.userService.remove(+id);
+    return response;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,15 +2,54 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto, ListUserDto } from './dto';
 import { IUserService } from './interface';
 
+let id = 1;
 @Injectable()
 export class UserService implements IUserService {
+  private users: Array<any>;
+  constructor() {
+    this.users = [];
+  }
+
   async create(createUserDto: CreateUserDto): Promise<any> {
-    throw new Error('Method not implemented.');
+    console.log(createUserDto);
+    this.users.push({ ...createUserDto, id });
+
+    id += 1;
+
+    return {
+      message: 'Usuario criado com sucesso',
+      user: { ...createUserDto },
+    };
   }
+
   listOne(id: ListUserDto): Promise<any> {
-    throw new Error('Method not implemented.');
+    const user = this.users.find((user) => user.id === +id);
+    return user;
   }
-  listAll(): Promise<any> {
-    throw new Error('Method not implemented.');
+
+  listAll(): any {
+    return this.users;
+  }
+
+  findAndUpdate(id: number, update: any): Promise<any> {
+    const user = this.users.find((user) => user.id === +id);
+
+    user.name = update.name;
+
+    console.log(user, 'aqui');
+
+    return user;
+  }
+
+  remove(id: number): any {
+    const newList = this.users.slice();
+
+    const position = newList.findIndex((user) => user.id === id);
+
+    newList.splice(position, 1);
+
+    this.users = newList;
+
+    return newList;
   }
 }
